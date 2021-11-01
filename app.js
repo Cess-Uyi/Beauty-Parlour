5e069446c432b8824694e9a8e6b894a102579e0a
cess-uyi <ebhoayesuccess@gmail.com>
2021-11-01 17:24:13 +0100

    trying to create a root route

--- app.js
+++ app.js
@@ -50,4 +50,10 @@ app.use("*", (req, res, next) => {
   next(error);
 });
 
+// app.use("*", (req, res, next) => {
+//   const error = new Error("Route not found");
+//   error.status = 404;
+//   next(error);
+// });
+
 module.exports = app;
