0f44d218d14ce3824a8525222b66435c781d12de
cess-uyi <ebhoayesuccess@gmail.com>
2021-11-02 13:45:46 +0100

    rearranging files

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-const http = require('http');
-const app = require('./app');
+const http = require("http");
+const app = require("./server/app");
 
 const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 
-server.listen(PORT, ()=> {
-    console.log(`SERVER RUNNING ON PORT: ${PORT}`)
+server.listen(PORT, () => {
+  console.log(`SERVER RUNNING ON PORT: ${PORT}`);
 });
-
